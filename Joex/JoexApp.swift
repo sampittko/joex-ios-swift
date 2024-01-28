53d46a0aa6458acc5d5d290869d6348f402decec
Samuel Pitoňák <sampittko@gmail.com>
2024-01-28 13:58:24 +0100

    Initial Commit

---
+++ Joex/JoexApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoexApp.swift
+//  Joex
+//
+//  Created by Samuel Pitoňák on 28/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct JoexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
