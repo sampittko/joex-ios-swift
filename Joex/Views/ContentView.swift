789e83b222ca75d65f85cdabd16d1fef59a08f6c
Samuel Pitonak <sampittko@gmail.com>
2024-02-04 20:50:53 +0100

    feat: face id

--- Joex/Views/ContentView.swift
+++ Joex/Views/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Samuel Pitoňák on 28/01/2024.
 //
 
+import LocalAuthentication
 import SwiftUI
 import SwiftData
 
@@ -23,92 +24,119 @@ struct ContentView: View {
     @State private var newNote: Bool = false
     @State private var newLogEntry: Bool = false
     @State private var note: String = ""
+    @State private var isUnlocked = false
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "A way to protect log entries you enter into the application."
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                // authentication has now completed
+                if success {
+                    isUnlocked = true
+                } else {
+                    // there was a problem
+                }
+            }
+        } else {
+            // no biometrics
+        }
+    }
     
     var body: some View {
-        NavigationStack {
-            ZStack(alignment: .bottom) {
-                List {
-                    ForEach(logEntries) { logEntry in
-                        LogEntryView(logEntry: logEntry)
-                            .swipeActions(allowsFullSwipe: false) {
-                                Button("Delete", systemImage: "trash", role: .destructive) {
-                                    modelContext.delete(logEntry)
-                                }
+        VStack {
+            if isUnlocked {
+                NavigationStack {
+                    ZStack(alignment: .bottom) {
+                        List {
+                            ForEach(logEntries) { logEntry in
+                                LogEntryView(logEntry: logEntry)
+                                    .swipeActions(allowsFullSwipe: false) {
+                                        Button("Delete", systemImage: "trash", role: .destructive) {
+                                            modelContext.delete(logEntry)
+                                        }
+                                    }
                             }
-                    }
-                }
-                
-                Button {
-                    newLogEntry = true
-                } label: {
-                    Image(systemName: "plus")
-                        .font(.system(size: 25).weight(.semibold))
-                        .padding(24)
-                        .background(Color.indigo)
-                        .foregroundColor(.white)
-                        .clipShape(Circle())
-                        .shadow(radius: 4, x: 0, y: 4)
-                }
-                .padding([.bottom], -5)
-                .confirmationDialog("Change background", isPresented: $newLogEntry) {
-                    Button("Note") { newNote = true }
-                    Button("Cancel", role: .cancel) { newLogEntry = false }
-                } message: {
-                    Text("Select type of new log entry")
-                }
-            }
-            .navigationTitle("Logs")
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button(action: {
-                        
-                    }, label: {
-                        Label("Migration", systemImage: "book.pages")
-                            .labelStyle(VerticalLabelStyle())
-                    })
-                    .disabled(logEntries.isEmpty)
-                    .accessibilityLabel("Migrate logs")
-                }
-            }
-            .sheet(isPresented: $newNote, onDismiss: {
-                newLogEntry = false
-                newNote = false
-            }, content: {
-                NavigationView {
-                    VStack {
-                        TextEditor(text: $note)
-                            .padding()
-                    }
-                    .toolbar {
-                        ToolbarItem(placement: .topBarLeading) {
-                            Button(action: {
-                                newLogEntry = false
-                                newNote = false
-                                note = ""
-                            }, label: {
-                                Text("Discard")
-                            })
-                            .accessibilityLabel("Discard note")
                         }
+                        
+                        Button {
+                            newLogEntry = true
+                        } label: {
+                            Image(systemName: "plus")
+                                .font(.system(size: 25).weight(.semibold))
+                                .padding(24)
+                                .background(Color.indigo)
+                                .foregroundColor(.white)
+                                .clipShape(Circle())
+                                .shadow(radius: 4, x: 0, y: 4)
+                        }
+                        .padding([.bottom], -5)
+                        .confirmationDialog("Change background", isPresented: $newLogEntry) {
+                            Button("Note") { newNote = true }
+                            Button("Cancel", role: .cancel) { newLogEntry = false }
+                        } message: {
+                            Text("Select type of new log entry")
+                        }
+                    }
+                    .navigationTitle("Logs")
+                    .toolbar {
                         ToolbarItem(placement: .topBarTrailing) {
                             Button(action: {
-                                let logEntryNote = LogEntry(note: note)
-                                modelContext.insert(logEntryNote)
-                                newLogEntry = false
-                                newNote = false
-                                note = ""
+                                
                             }, label: {
-                                Text("Save")
+                                Label("Migration", systemImage: "book.pages")
+                                    .labelStyle(VerticalLabelStyle())
                             })
-                            .disabled(note.isEmpty)
-                            .buttonStyle(.borderedProminent)
-                            .buttonBorderShape(.capsule)
-                            .accessibilityLabel("Save note")
+                            .disabled(logEntries.isEmpty)
+                            .accessibilityLabel("Migrate logs")
                         }
                     }
+                    .sheet(isPresented: $newNote, onDismiss: {
+                        newLogEntry = false
+                        newNote = false
+                    }, content: {
+                        NavigationView {
+                            VStack {
+                                TextEditor(text: $note)
+                                    .padding()
+                            }
+                            .toolbar {
+                                ToolbarItem(placement: .topBarLeading) {
+                                    Button(action: {
+                                        newLogEntry = false
+                                        newNote = false
+                                        note = ""
+                                    }, label: {
+                                        Text("Discard")
+                                    })
+                                    .accessibilityLabel("Discard note")
+                                }
+                                ToolbarItem(placement: .topBarTrailing) {
+                                    Button(action: {
+                                        let logEntryNote = LogEntry(note: note)
+                                        modelContext.insert(logEntryNote)
+                                        newLogEntry = false
+                                        newNote = false
+                                        note = ""
+                                    }, label: {
+                                        Text("Save")
+                                    })
+                                    .disabled(note.isEmpty)
+                                    .buttonStyle(.borderedProminent)
+                                    .buttonBorderShape(.capsule)
+                                    .accessibilityLabel("Save note")
+                                }
+                            }
+                        }
+                    })
                 }
-            })
-        }
+            }
+            else {
+                Text("Locked")
+            }
+        }.onAppear(perform: authenticate)
     }
 }
 
