0b8fef66e1043422848a9c36b0b6eff7d583f9ba
Samuel Pitonak <sampittko@gmail.com>
2024-02-17 19:03:50 +0100

    feat: daily migration reminder setting for notification

--- Joex/Views/Settings/SettingsView.swift
+++ Joex/Views/Settings/SettingsView.swift
@@ -33,25 +33,48 @@ struct SettingsView: View {
     private var deleteMigratedLogAfter: String = DeleteMigratedLogAfter.ThreeDays.rawValue
     @AppStorage("authenticationTimeout")
     private var authenticationTimeout: String = AuthenticationTimeout.Immediately.rawValue
+    @AppStorage("requireAuthentication")
+    private var requireAuthentication: Bool = true
+    @AppStorage("dailyMigrationReminder")
+    private var dailyMigrationReminder: Bool = false
+    @AppStorage("dailyMigrationReminderTime")
+    private var dailyMigrationReminderTime: TimeInterval = Date.now.timeIntervalSinceReferenceDate
+    @State private var dailyMigrationReminderTimeState = Date.now
     
     var body: some View {
         Form {
+            Section("Privacy") {
+                Toggle("Require authentication", isOn: $requireAuthentication)
+                    .disabled(true)
+                if requireAuthentication {
+                    Picker("Lock app after", selection: $authenticationTimeout) {
+                        ForEach(AuthenticationTimeout.allCases, id: \.self) { value in
+                            Text(value.rawValue).tag(value.rawValue)
+                        }
+                    }
+                }
+            }
             Section("Migration") {
                 Picker("Delete log after", selection: $deleteMigratedLogAfter) {
                     ForEach(DeleteMigratedLogAfter.allCases, id: \.self) { value in
                         Text(value.rawValue).tag(value.rawValue)
                     }
                 }
-            }
-            Section("Privacy") {
-                Picker("Lock app after", selection: $authenticationTimeout) {
-                    ForEach(AuthenticationTimeout.allCases, id: \.self) { value in
-                        Text(value.rawValue).tag(value.rawValue)
-                    }
+                Toggle("Daily reminder", isOn: $dailyMigrationReminder)
+//                    .disabled(true)
+                if dailyMigrationReminder {
+                    DatePicker("Reminder time", selection: $dailyMigrationReminderTimeState , displayedComponents: .hourAndMinute)
+                        .onChange(of: dailyMigrationReminderTimeState) { _, newDate in
+                            dailyMigrationReminderTime = newDate.timeIntervalSinceReferenceDate
+                        }
                 }
             }
         }
         .navigationTitle("Settings")
+        // Workaround from: https://www.hackingwithswift.com/forums/swiftui/dates-are-hard/24218/24246
+        .onAppear {
+            dailyMigrationReminderTimeState = Date(timeIntervalSinceReferenceDate: dailyMigrationReminderTime)
+        }
         .onDisappear {
             updateMigratedLogsList(deleteMigratedLogAfter: deleteMigratedLogAfter, logEntries: logEntries, modelContext: modelContext)
         }
